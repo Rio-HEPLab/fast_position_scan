294a04a841e808cd6e10395d27bdf3c3812fe3eb
Vinícius Fuchshuber <vinifux@gmail.com>
2023-12-08 10:36:45 -0300

    first commit

---
+++ fastPositionScan/fastPositionScan/Classes/QDC.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace fastPositionScan
+{
+    internal class QDC
+    {
+        [DllImport("QDClibrary.dll")]
+        public static extern void helloWorld();
+
+        [DllImport("QDClibrary.dll")]
+        public static extern int add(int a, int b);
+
+        [DllImport("QDClibrary.dll")]
+        public static extern void QDC_Init();
+
+        [DllImport("QDClibrary.dll")]
+        public static extern void QDC_End();
+
+        [DllImport("QDClibrary.dll")]
+        public static extern int QDC_Read();
+
+        [DllImport("QDClibrary.dll")]
+        public static extern int Read(int numeroCiclos);
+    }
+}
